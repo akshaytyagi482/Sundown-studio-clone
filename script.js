eececbf38c869af9e702f4f7326942326f262299
Akshay kumar tyagi <79367523+akshaytyagi482@users.noreply.github.com>
2024-01-19 15:32:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+function imggenerator(){
+    let ok = document.querySelector("#fixed-img")
+let hehe = document.querySelectorAll(".elem")
+
+hehe.forEach(function(e){
+    e.addEventListener('mouseenter', function(){
+        let b = e.getAttribute("data-img")
+        ok.style.display = "block"; 
+         ok.style.backgroundImage = `url(${b})`
+    })
+})
+
+hehe.forEach(function(e){
+    e.addEventListener('mouseleave',() => {
+          ok.style.display = "none";
+    })
+})
+
+}
+function sliderprop(){
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        mousewheel: true,
+        keyboard: true,
+        slidesPerView: "auto",
+        freeMode: true,
+      });
+}
+
+    // let pointer = document.querySelector("#custom-c")
+    // let swip = document.querySelector(".swiper-wrapper")
+    // swip.addEventListener("mouseenter", (event) => {
+    //         pointer.style.display = "block";
+    // })
+    // swip.addEventListener("mouseleave", (event) => {
+    //     pointer.style.display = "none";
+    //  })
+    //  swip.addEventListener("mousemove",(evt) => {
+    //     let x = evt.clientX - 45;
+    //     let y = evt.clientY - 45;
+    //        pointer.style.left = `${x}px`
+    //        pointer.style.top = `${y}px`
+    //  })
+sliderprop()
+imggenerator()
